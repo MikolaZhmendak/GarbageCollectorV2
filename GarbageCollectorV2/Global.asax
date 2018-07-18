da53cfb456dd9179d8e57a671a2f8d9c17b9aeca
MikolaZhmendak <zhmendakmikola@yahoo.com>
2018-07-18 08:38:29 -0500

    Add project files.

---
+++ GarbageCollectorV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GarbageCollectorV2.MvcApplication" Language="C#" %>
---
+++ GarbageCollectorV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarbageCollectorV2.Startup))]
+namespace GarbageCollectorV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
